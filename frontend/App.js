1823c7a5d5401ac47934180c84846747479b6dfa
EthanTGo <ethango1997@gmail.com>
2020-03-04 20:54:35 -0500

    added more file

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Link } from 'react-native';
+import SignUp from './SignUp'
+import TermsAndConditions from './TermsOfService'
+import EmailVer from './EmailVerification'
+import PhoneVer from './PhoneVerification'
+import SignLogIn from './SignLogIn'
 
 // Style part of this apge
 const styles = StyleSheet.create({
@@ -34,9 +38,8 @@ const styles = StyleSheet.create({
 export default class Terms extends Component {
   // state to identify whether a user has accepted the terms of service
   state = {
-    accepted: false
+    accepted: true
   }
-
 
   onPress = () => {
     this.setState({
@@ -45,51 +48,19 @@ export default class Terms extends Component {
   }
 
   render() {
-    return (
-        <ScrollView style={styles.container}>
-        <Text style={styles.heading}> Terms of Service!</Text>
-        <Text style={styles.container} > Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ac tincidunt vitae semper quis lectus nulla. Eu sem integer vitae justo 
-          eget magna fermentum. In egestas erat imperdiet sed euismod nisi porta lorem. 
-          Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. 
-          Cras adipiscing enim eu turpis egestas. Odio ut sem nulla pharetra. Sit amet 
-          facilisis magna etiam tempor orci eu lobortis elementum. Massa vitae tortor 
-          condimentum lacinia quis vel eros donec ac. Malesuada nunc vel risus commodo. 
-          Tellus cras adipiscing enim eu turpis egestas pretium.
-          Eu lobortis elementum nibh tellus molestie nunc. Praesent semper feugiat nibh 
-          sed pulvinar proin gravida. Sit amet venenatis urna cursus eget. Aenean et tortor 
-          at risus viverra adipiscing at. Turpis egestas pretium aenean pharetra magna ac. 
-          Nisl tincidunt eget nullam non. Facilisi morbi tempus iaculis urna id. Tortor 
-          itae purus faucibus ornare suspendisse sed. Aliquam faucibus purus in massa tempor.
-           Eu mi bibendum neque egestas. Elit duis tristique sollicitudin nibh sit amet commodo
-            nulla. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium.
-        </Text>
-        <Text style={styles.container} > Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ac tincidunt vitae semper quis lectus nulla. Eu sem integer vitae justo 
-          eget magna fermentum. In egestas erat imperdiet sed euismod nisi porta lorem. 
-          Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. 
-          Cras adipiscing enim eu turpis egestas. Odio ut sem nulla pharetra. Sit amet 
-          facilisis magna etiam tempor orci eu lobortis elementum. Massa vitae tortor 
-          condimentum lacinia quis vel eros donec ac. Malesuada nunc vel risus commodo. 
-          Tellus cras adipiscing enim eu turpis egestas pretium.
-          Eu lobortis elementum nibh tellus molestie nunc. Praesent semper feugiat nibh 
-          sed pulvinar proin gravida. Sit amet venenatis urna cursus eget. Aenean et tortor 
-          at risus viverra adipiscing at. Turpis egestas pretium aenean pharetra magna ac. 
-          Nisl tincidunt eget nullam non. Facilisi morbi tempus iaculis urna id. Tortor 
-          itae purus faucibus ornare suspendisse sed. Aliquam faucibus purus in massa tempor.
-           Eu mi bibendum neque egestas. Elit duis tristique sollicitudin nibh sit amet commodo
-            nulla. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium.
-        </Text>
-      
-        <TouchableOpacity style={styles.button} onPress={ ()=>alert("Terms and conditions accepted") & this.onPress() }><Text>Accept</Text></TouchableOpacity>
-      
-      
-      
-      </ScrollView>
+    let acceptedTerms = this.state.accepted;
+    if(!acceptedTerms){
+      return (
+        <TermsAndConditions></TermsAndConditions>
+      );
+    }
+    else{
+      return(
+        <SignLogIn></SignLogIn>
+      );
+    }
 
-    );
   }
 }
+
 
---
+++ frontend/EmailVerification.js
@@ -0,0 +1,106 @@
+
+import React from 'react'
+import {
+  View,
+  Button,
+  TextInput,
+  StyleSheet,
+  Image
+} from 'react-native'
+
+class EmailVer extends React.Component {
+  state = {
+    email: '', confemail: ''
+  }
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
+  }
+
+  // After e-mail verification, we need to set up phone verification
+  signUp = async () => {
+    const { email, confemail } = this.state
+    try {
+      // here place your signup logic
+      console.log('Next adding phone number!: ', success)
+    } catch (err) {
+      console.log('error signing up: ', err)
+    }
+  }
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+        <Image
+        style={styles.image}
+        source={require('./verto_logo.png')}
+      />
+        </View>
+
+      <View style={styles.otherInput}>
+        <TextInput
+          style={styles.input}
+          placeholder='e-mail'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('email', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='confirm e-mail'
+          secureTextEntry={true}
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('confemail', val)}
+        />
+        <Button
+          title='Confirm Phone Number'
+          onPress={this.signUp}
+        />
+        </View>
+        
+      </View>
+    )
+  }
+}
+
+/*
+  TODO: Implement a function to adjust text size upon typing
+
+*/
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 50,
+    backgroundColor: '#42A5F5',
+    margin: 20,
+    padding: 8,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  header:{
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    aspectRatio: 0.8, 
+    resizeMode: 'contain',
+
+  }
+  , otherInput : {
+    flex: 8,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default EmailVer;
