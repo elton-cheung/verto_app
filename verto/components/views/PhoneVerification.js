026259b4d12e8c1c43f5c51f454759234f6c6524
EthanTGo <ethango1997@gmail.com>
2020-04-09 20:09:19 -0400

    modified PhoneVer

--- verto/components/views/PhoneVerification.js
+++ verto/components/views/PhoneVerification.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import {View, Button, TextInput, StyleSheet, Image} from 'react-native';
+import {View, Button, TextInput, StyleSheet, Image, Text, Linking} from 'react-native';
+import { Icon } from 'react-native-elements';
+// import styles from './style/signUpFlowStyle.js';
 
 class PhoneVer extends React.Component {
   state = {
     email: '',
     confemail: '',
   };
+  
   onChangeText = (key, val) => {
     this.setState({[key]: val});
   };
@@ -21,6 +25,11 @@ class PhoneVer extends React.Component {
     }
   };
 
+  completeEmail() {
+    // this.signUp;
+    this.props.navigation.navigate('PhoneVer');
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -31,28 +40,45 @@ class PhoneVer extends React.Component {
           />
         </View>
 
-        <View style={styles.otherInput}>
-          <TextInput
-            style={styles.input}
-            placeholder="Phone Number"
-            autoCapitalize="none"
-            placeholderTextColor="grey"
-            onChangeText={val => this.onChangeText('phone', val)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Confirm Phone Number"
-            secureTextEntry={true}
-            autoCapitalize="none"
-            placeholderTextColor="grey"
-            onChangeText={val => this.onChangeText('confphone', val)}
-          />
-          <Button
-            title="Finish Sign-Up"
-            // For now I'm gonna let it navigate to Splash Screen -Jennie
-            onPress={() => this.props.navigation.navigate('Redirect')}
+
+
+        <View style={styles.body}>
+        <View style={styles.header}>
+          <Image
+            style={styles.image2}
+            source={require('../../assets/images/Placeholder.jpg')}
           />
         </View>
+
+        <Text style={{textAlign: 'center', fontSize: 15}}>Phone Number Verification</Text>
+        <TextInput
+          textAlign={'center'}
+            style={styles.input}
+            placeholder="6 Digit"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholderTextColor="grey"
+            onChangeText={val => this.onChangeText('password', val)}
+            maxLength={6}
+          />
+          <Text style={{textAlign: 'center', fontSize: 15}}>An SMS message has been sent</Text>
+          <Text style={{textAlign: 'center', fontSize: 15}}>to your mobile phone number</Text>
+          <Text style={{textAlign: 'center', fontSize: 15}}>Please enter the 6 digit phone number</Text>
+        </View>
+
+        <View style={styles.otherInput}>
+          <Button
+            title="Verify Number"
+            onPress={() => this.props.navigation.navigate('LogIn')}
+          />
+                    <Text style={{textAlign: 'center', fontSize: 15}}>If you have not received the SMS,</Text>
+<Text style={{color: 'blue'}}
+      onPress={() => alert("SMS sent")}>
+  click here
+</Text>
+        </View>
+        
+
       </View>
     );
   }
@@ -62,42 +88,101 @@ class PhoneVer extends React.Component {
   TODO: Implement a function to adjust text size upon typing
 
 */
-
 const styles = StyleSheet.create({
+  buttonLog: {
+      alignSelf: 'stretch',
+      backgroundColor: "#4d94ff",
+      margin: 20,
+      padding: 8,
+      color: '#F5F5F5',
+      borderRadius: 14,
+      fontSize: 18,
+      fontWeight: '500',
+      height: 50,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonSign: {
+      alignSelf: 'stretch',
+      backgroundColor: '#ffda5c',
+      margin: 20,
+      padding: 8,
+      color: '#ffffff',
+      borderRadius: 14,
+      fontSize: 18,
+      fontWeight: '500',
+      height: 50,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
   input: {
-    width: 290,
-    height: 45,
-    backgroundColor: '#fff',
-    margin: 15,
-    padding: 12,
-    color: 'black',
-    borderRadius: 30,
-    fontSize: 14,
-    fontWeight: '400',
-    borderColor: 'grey',
-    borderWidth: 1.5
-  },
-  header: {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  image: {
-    aspectRatio: 0.9,
-    resizeMode: 'contain',
-  },
-  otherInput: {
-    flex: 4,
-    alignItems: 'flex-start',
-    // justifyContent: 'center',
-    alignItems: 'center',
-  },
+      width: 290,
+      height: 45,
+      backgroundColor: '#fff',
+      margin: 15,
+      padding: 12,
+      color: 'black',
+      borderRadius: 30,
+      fontSize: 14,
+      fontWeight: '400',
+      borderColor: 'grey',
+      borderWidth: 1.5
+    },
+    header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    body: {
+      flex: 8,
+      justifyContent: 'center'
+    },
+    container: {
+      flex: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white'
+    },
+    miniInput: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    otherInput: {
+      flex: 2,
+      alignItems: 'flex-start',
+      // justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      aspectRatio: 0.9,
+      resizeMode: 'contain',
+    },
+    image2: {
+      aspectRatio: 0.4,
+      resizeMode: 'contain',
+    },
+    logInInput: {
+      flex: 1,
+      backgroundColor: 'white',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      alignSelf: 'stretch',
+    },
+    signInInput: {
+      flex: 1,
+      backgroundColor: 'white',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      alignSelf: 'stretch',
+    },
+    blank: {
+      flex: 2,
+    }
 });
+
 
 export default PhoneVer;
